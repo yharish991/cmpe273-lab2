e514a0349b0ca50c159e15dd600e3e2c458d9d59
Harish Yayi <yharish991@gmail.com>
2014-09-17 07:06:47 -0700

    commit

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ function del(request, response) {
 function put(request, response) {
 	console.log("PUT:: Re-generate new seesion_id for the same user");
 	// TODO: refresh session id; similar to the post() function
+	//
 	var name=request.body.name;
 	var email=request.body.email;
 	var newSessionId = login.login(name,email);
